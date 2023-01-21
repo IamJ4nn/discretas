85d547ea399f6b38c39e4822a9587627c3a9be8a
Janer Vega <janervega399@gmail.com>
2023-01-21 12:53:49 -0500

    Agregados los metodos

--- lista.h
+++ lista.h
@@ -3,13 +3,26 @@
 
 #include <stdio.h>
 
-typedef struct Nodo {
+typedef struct Nodo
+{
     char letra;
-    struct Nodo* siguiente;
+    struct Nodo *siguiente;
 } Nodo;
 
-typedef struct Lista {
-    Nodo* cabeza;
+typedef struct Lista
+{
+    Nodo *cabeza;
+    int longitud;
 } Lista;
+
+Nodo *CrearNodo(char letra);
+void DestruirNodo(Nodo *nodo);
+void InsertarPrincipio(Lista *lista, char letra);
+void InsertarFinal(Lista *lista, char letra);
+void InsertarDespues(int n, Lista *lista, char letra);
+int Contar(Lista *lista);
+int EstaVacia(Lista *lista);
+void EliminarPrincipio(Lista *lista);
+void EliminarUltimo(Lista *lista);
 
 #endif /* lista_h */
